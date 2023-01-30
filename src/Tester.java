ab1fac757f201b4725bc219f68986ed955890491
Eduard <eduard.prog22@gmail.com>
2023-01-30 22:37:45 +0100

    Second try

--- src/Tester.java
+++ src/Tester.java
@@ -1,6 +1,7 @@
 import java.time.LocalDateTime;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.time.Year;
 
 
 public class Tester {
@@ -9,21 +10,16 @@ public class Tester {
     public static void main(String[] args){
 
         LocalDate now = LocalDate.now();
-        long days = ChronoUnit.DAYS.between(now, LocalDate.of(2024, 1, 1));
 
-        LocalDate nextMonth = LocalDate.of(2023, 2 , 1);
+        int endOfMonth = now.lengthOfMonth() - now.getDayOfMonth();
+        System.out.printf("End of month comes in: %d day/s!\n", endOfMonth);
 
+       /* int ca = now.getDayOfYear();
+        int c = Year.of(now.getYear()).length();
+        System.out.println(c + " "+ ca);*/
 
-        System.out.println("Days till end of month: " + (nextMonth.getDayOfYear() - now.getDayOfYear()));
-        System.out.println("Days till end of the year: " + days);
-
-
-
-
-
-
-
-
+        int endOfYear = Year.of(now.getYear()).length() - now.getDayOfYear();
+        System.out.printf("End of the year comes in: %d day/s!\n", endOfYear);
 
     }
 }
